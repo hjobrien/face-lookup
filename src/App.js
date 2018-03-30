e0f25277978cecb29a173df00a21a117430b1cbf
Hank O'Brien <hank.obrien900@icloud.com>
2018-03-29 17:28:36 -0700

    converted main application to grid

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { MyFirstGrid } from './grid.js'
+
 import './css/App.css';
+import './css/react-grid-styles.css';
+import './css/resizable-style.css';
+
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      return (
+          <MyFirstGrid/>
+          // <div className="App">
+          //     <header className="heading">
+          //         <h1 className="title">FaceLookup</h1>
+          //     </header>
+          //
+          // </div>
+      )
+    // return (
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <h1 className="App-title">Welcome to React</h1>
+    //     </header>
+    //     <p className="App-intro">
+    //       To get started, edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //   </div>
+    // );
   }
 }
+
 
 export default App;
