4caccd73e652b547bf9d74b007693c33c392da42
Hank O'Brien <hank.obrien900@icloud.com>
2018-03-30 18:07:48 -0700

    added webcam view, improved grid styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { MyFirstGrid } from './grid.js'
+// import { Grid } from './grid.js'
+import GridLayout from 'react-grid-layout'
+import Webcam from 'react-webcam';
 
 import './css/App.css';
 import './css/react-grid-styles.css';
@@ -7,28 +9,73 @@ import './css/resizable-style.css';
 
 
 class App extends Component {
-  render() {
-      return (
-          <MyFirstGrid/>
-          // <div className="App">
-          //     <header className="heading">
-          //         <h1 className="title">FaceLookup</h1>
-          //     </header>
-          //
-          // </div>
-      )
-    // return (
-    //   <div className="App">
-    //     <header className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <h1 className="App-title">Welcome to React</h1>
-    //     </header>
-    //     <p className="App-intro">
-    //       To get started, edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //   </div>
-    // );
-  }
+
+    setRef = (webcam) => {
+        this.webcam = webcam;
+    }
+
+    capture = () => {
+        const imageSrc = this.webcam.getScreenshot();
+    };
+    render() {
+        const mainLayout = [
+            {i: 'camera', x: 0, y: 0, w: 4, h: 11, static: false},
+            {i: 'b', x: 4, y: 0, w: 5, h: 7},
+            {i: 'c', x: 0, y: 5, w: 1, h: 3},
+            {i: 'filler', x: 4, y: 0, w: 1, h: 2},
+            {i: 'login', x: 1, y: 0, w: 3, h: 3}
+
+        ];
+
+        const cameraLayout = [
+            {i: 'webcam', x: 0, y: 0, w: 4, h: 10, static: true},
+            {i: 'screenshot', x: 0, y: 10, w: 4, h: 1, static: true},
+
+        ];
+
+        function loginHandler(){
+            console.log("you tried to log in")
+        }
+
+
+        return (
+            <div>
+                <header className="heading">
+                    <h1 className="Title">Title</h1>
+                </header>
+                <GridLayout className="layout" layout={mainLayout} margin={[0,0]} cols={12} rowHeight={30} width={1200}>
+                    <div key="camera">
+                        <GridLayout className="layout" layout={cameraLayout} margin={[0,0]} cols={12} rowHeight={30} width={1200}>
+                            <div key="webcam" className="webcamComponent innerGridItem ">
+                                <Webcam className="webcam"
+                                        audio={false}
+                                        width={400}
+                                        ref={this.setRef}/>
+                            </div>
+                            <div key="screenshot" className="innerGridItem webcamComponent">
+                                <button className="uiButton"
+                                        onClick={this.capture}>Capture photo</button>
+                            </div>
+                        </GridLayout>
+
+                    </div>
+                    <div key="b">b</div>
+
+                    <div key="c">c</div>
+
+                    <div key="filler">filler</div>
+
+                    <div key="login">
+                        <button onClick={loginHandler} className="uiButton">
+                            Login
+                        </button>
+                    </div>
+
+
+                </GridLayout>
+            </div>
+        )
+    }
 }
 
 
--- src/grid.js
+++ src/grid.js
@@ -1,11 +1,11 @@
 /**
  * Created by Hank on 3/29/18.
  */
-import React, { Component } from 'react';
+import React  from 'react';
 
 import GridLayout from 'react-grid-layout';
 
-export class MyFirstGrid extends React.Component {
+export class Grid extends React.Component {
     render() {
         // layout is an array of objects, see the demo for more complete usage
         var layout = [
@@ -15,7 +15,13 @@ export class MyFirstGrid extends React.Component {
         ];
         return (
             <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
-                <div key="a">a</div>
+                <div key="a">a
+                    <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+                        <div key="e">e</div>
+                        <div key="f">f</div>
+                        <div key="g">g</div>
+                    </GridLayout>
+                </div>
                 <div key="b">b</div>
                 <div key="c">c</div>
             </GridLayout>
